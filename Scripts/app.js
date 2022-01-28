0d45b163831fdeac94ee44a416f97a35dcb50415
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-28 18:01:15 -0500

    updated live site for ICE3

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,13 @@
 // IIFE -- Immediately Invoked Function Express
 // AKA anonymous self-executing function
+
+"use strict";
 (function()
 {
     function DisplayHome()
     {
+        console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
@@ -35,15 +39,82 @@
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
 
+    }
 
-        // example of insert before
-        //MainContent.before(MainParagraph);
+    function DisplayAboutPage()
+    {
+        console.log("About Us Page");
+    }
 
-        // example of deletion
-        //document.getElementById("AboutUsButton").remove();
-        //AboutUsButton.remove();
+    function DisplayProjectsPage()
+    {
+        console.log("Our Projects Page");
+    }
 
-        // ES6 and HTLM5 => Template Strings => "Super Strings"
+    function DisplayServicesPage()
+    {
+        console.log("Our Services Page");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function()
+        {
+            if(subscribeCheckbox.checked)
+            { 
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container -> add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts how many keys
+
+            // for every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create a new empty contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
 
@@ -52,13 +123,26 @@
     {
         console.log("App Started!!");
 
-        switch(document.title)
-        {
-            case "Home":
-                DisplayHome();
-                break;
+        switch (document.title) {
+          case "Home":
+            DisplayHome();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Projects":
+            DisplayProjectsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
         }
-        
     }
     
 
